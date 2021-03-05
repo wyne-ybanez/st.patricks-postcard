39c48ff920d547792cfdc63db020f5820a9b0376
wyne-ybanez <wyne_01@icloud.com>
2021-03-05 13:32:32 +0000

    Added 5 new st.Patricks day questions, need to add a smal bit more functionality

--- quiz.js
+++ quiz.js
@@ -35,6 +35,9 @@ function showQuestion(question) {
     const button = document.createElement('button')
     button.innerText = answer.text
     button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
     button.addEventListener('click', selectAnswer)
     answerButtonsElement.appendChild(button)
   })
@@ -51,61 +54,79 @@ function resetState() {
 // Allows user to choose an answer
 function selectAnswer(e) {
   const selectedButton = e.target
-
-  // Highlights the selected answer
-  for (var i = 0; i < btns.length; i++) {
-    btns[i].addEventListener("click", ()=>{
-    var current = document.getElementsByClassName("active");
-   // If there's no active class - class changes to nothing
-    if (current.length > 0) {
-      current[0].className = current[0].className.replace(" active", "");
-    }
-   // Add the active class to the current/clicked button
-    this.className += " active";
-    });
-  }
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
   
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
-    this.className += " active";
   } else {
     startButton.innerText = 'Restart'
     startButton.classList.remove('hide')
-    this.className += " active";
   }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+    btns.innerText = 'Wrong'
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
 }
 
 const questions = [
   {
-    question: 'What is 2 + 2?',
+    question: 'Where was St. Patrick born?',
     answers: [
-      { text: '4', correct: true },
-      { text: '22', correct: false }
+      { text: 'Ireland', correct: false },
+      { text: 'Britain', correct: true },
+      { text: 'Wales', correct: false },
+      { text: 'Scotland', correct: false }
     ]
   },
   {
-    question: 'Who is the best YouTuber?',
+    question: "In the 1600s the first St.Patrick's day celebrated - but where was it held?",
     answers: [
-      { text: 'Web Dev Simplified', correct: true },
-      { text: 'Traversy Media', correct: true },
-      { text: 'Dev Ed', correct: true },
-      { text: 'Fun Fun Function', correct: true }
+      { text: 'St. Augustine, Florida', correct: true },
+      { text: 'Boston', correct: false },
+      { text: 'New York City', correct: false },
+      { text: 'Dublin', correct: false }
     ]
   },
   {
-    question: 'Is web development fun?',
+    question: 'What plant or leaf did St.Patrick use as a metaphor for Christianity?',
     answers: [
-      { text: 'Kinda', correct: false },
-      { text: 'YES!!!', correct: true },
-      { text: 'Um no', correct: false },
-      { text: 'IDK', correct: false }
+      { text: 'Neem', correct: false },
+      { text: 'Maple Leaf', correct: false },
+      { text: 'Shamrock', correct: true },
+      { text: 'Oxalis', correct: false }
     ]
   },
   {
-    question: 'What is 4 * 2?',
+    question: "What was St.Patrick's original name (name at birth)?",
     answers: [
-      { text: '6', correct: false },
-      { text: '8', correct: true }
+      { text: 'Robert Scitte', correct: false },
+      { text: 'Patricae Fulton', correct: false },
+      { text: 'Patricious Eastaughffe', correct: false },
+      { text: 'Maewyn Succat', correct: true }
+    ]
+  },
+  {
+    question: "What was St.Patrick's original name (name at birth)?",
+    answers: [
+      { text: 'Robert Scitte', correct: false },
+      { text: 'Patricae Fulton', correct: false },
+      { text: 'Patricious Eastaughffe', correct: false },
+      { text: 'Maewyn Succat', correct: true }
     ]
   }
 ]

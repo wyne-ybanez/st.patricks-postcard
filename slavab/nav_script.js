fc6b44b970932b7a43cec9bb187947b5bb54c3ea
slavabdev <slav.balykov@gmail.com>
2021-03-06 22:18:49 +0000

    added switchmode and resp nav

--- slavab/nav_script.js
+++ slavab/nav_script.js
@@ -1,5 +1,29 @@
 $('document').ready(function(){
     $('.burger').click(function (event){
         $('.burger,.menu').toggleClass('active');
+        $('body').toggleClass('lock');
     });
 });
+ 
+
+    $('body').toggleClass(localStorage.toggled);
+    function paddyLight() {
+ 
+  if (localStorage.toggled != 'paddy_green') {
+    $('body, p, h2').toggleClass('paddy_green', true);
+    localStorage.toggled = "paddy_green";
+     
+  } else {
+    $('body, p, h2').toggleClass('paddy_green', false);
+    localStorage.toggled = "";
+  }
+}
+if ($('body').hasClass('paddy_green')) {
+   $( '#checkBox' ).prop( "checked", true )
+} else {
+  $( '#checkBox' ).prop( "checked", false )
+}
+
+
+
+
